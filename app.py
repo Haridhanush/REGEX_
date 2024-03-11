457327ceb124c7c65669776a39470721c32b3557
Haridhanush Ravichandran <81855991+Haridhanush@users.noreply.github.com>
2024-03-11 05:59:19 +0000

    Ready for deployment

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def results():
     matches = re.findall(regex_pattern, test_string)
     return render_template('index.html', test_string=test_string, regex_pattern=regex_pattern, matches=matches)
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port=5000)
+    app.run(debug=False,host="0.0.0.0",port=5000)
